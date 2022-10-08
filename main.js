9200c1bcdaffd8188a8153660a9cd7e818dbce70
Gabriel-LVB <gabrielvieiraxc12@gmail.com>
2022-10-07 22:03:38 -0300

    renaming elements to clear code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let score = {
     user: 0,
     pc: 0,
-    board: () => {
+    scoreBoard: () => {
         console.log("-----Scoreboard-----")
         console.log(`You: ${score.user}`)
         console.log(`Computer: ${score.pc}`)
@@ -18,36 +18,37 @@ let result = {
     tie: () => {
         console.log("It's a tie!")
     },
-    win: () => {
+    playerWon: () => {
         console.log(`${choice.user === "R" ? "Rock" : choice.user === "P" ? "Paper" : "Scissors"} beats ${choice.pc === "R" ? "Rock" : choice.pc === "P" ? "Paper" : "Scissors"}`);
         console.log("You won this round!")
         score.user++
     },
-    lose: () => {
+    playerLost: () => {
         console.log(`${choice.pc === "R" ? "Rock" : choice.pc === "P" ? "Paper" : "Scissors"} beats ${choice.user === "R" ? "Rock" : choice.user === "P" ? "Paper" : "Scissors"}`);
         console.log("You lost this round!")
         score.pc++
     }
 }
 
-const ask = () => {return prompt("What is your choice? (R)ock / (P)aper / (S)cissors").trim().toUpperCase()}
+const askPlayerChoice = () => {
+    return prompt("What is your choice? (R)ock / (P)aper / (S)cissors").
+    trim().
+    toUpperCase()}
 
-let cont
-
-function user(){
-    choice.user = ask()
+function userChoice(){
+    choice.user = askPlayerChoice()
     while(choice.user !== "R" && choice.user !== "P" && choice.user !== "S"){
         cont++
         if (cont === 5){
             break
         }
         alert("Please enter a valid value")
-        choice.user = ask()
+        choice.user = askPlayerChoice()
     }
     console.log(`Your choice: ${choice.user}`)
 }
 
-function pc(){
+function computerChoice(){
     const calc = Math.floor(Math.random() * 3)
     choice.pc = (calc === 0 ? "R" : calc === 1 ? "P" : "S")
     console.log(`Computer choice: ${choice.pc}`)
@@ -62,24 +63,24 @@ function comparation({user, pc}){
         user === "S" && pc === "P" ||
         user === "P" && pc === "R"
     ){
-        result.win()
+        result.playerWon()
     }else{
-        result.lose()
+        result.playerLost()
     }
-    score.board()
+    score.scoreBoard()
     choice.user = null
     choice.pc = null
 }
 
-function round(){
-    user()
-    pc()
+function nextRound(){
+    userChoice()
+    computerChoice()
     comparation(choice)
 }
 
 function game(){
     while (score.user < 5 && score.pc < 5){
-        round()
+        nextRound()
     }
     if (score.user === 5) {
         console.log("Congrats!! You Won!")
